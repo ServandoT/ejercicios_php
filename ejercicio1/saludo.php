52c1951418b861382fee57eafdda0f2f5df352be
ServandoT <gonzalez.agar.carlos@iescamas.es>
2024-09-21 12:27:41 +0200

    Estilar página de destino
    Añadido el estilo a la página de destino usando una estructura html además de php

--- ejercicio1/saludo.php
+++ ejercicio1/saludo.php
@@ -1,6 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+    <link rel="stylesheet" href="/CSS/style.css">
+</head>
+
+<body>
+    <?php
     $nombre = $_GET["nombre"];
     $apellido = $_GET["apellido"];
 
-    echo "Hola $nombre $apellido";
-?>
+    echo "<p>Hola $nombre $apellido </p>";
+    ?>
+</body>
+
+</html>
